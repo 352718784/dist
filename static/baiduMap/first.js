fb42bc00da8669ccdbd9fa69444ed121fefa5566
huangshengqiang <huangshengqiang@kedacom.com>
2020-06-06 16:55:19 +0800

    文件打包测试

---
+++ static/baiduMap/first.js
@@ -0,0 +1,1 @@
+(function(){ window.BMap_loadScriptTime = 1589536928748; document.write('<script type="text/javascript" src="/static/baiduMap/second.js"></script>');})();
---
+++ static/demo.js
@@ -0,0 +1,7 @@
+const demoAlert = () => {
+  alert('demo')
+}
+alert('demo-zj')
+export {
+  demoAlert
+}
