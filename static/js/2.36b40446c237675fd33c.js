4c9befe57f5d57e898ec81a46c8dee768480bf58
huangshengqiang <huangshengqiang@kedacom.com>
2020-07-03 15:20:03 +0800

    dev 更新

--- static/js/2.36b40446c237675fd33c.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([2],{"+UnO":function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var a={render:function(){var e=this.$createElement;return(this._self._c||e)("div",{staticClass:"mainPage"},[this._v("\n  主页\n")])},staticRenderFns:[]};var r=t("VU/8")({name:"mainPage",data:function(){return{catalog:[]}}},a,!1,function(e){t("KHJr")},"data-v-09fef6e0",null);n.default=r.exports},KHJr:function(e,n){}});
-//# sourceMappingURL=2.36b40446c237675fd33c.js.map
